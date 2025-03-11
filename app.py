fd39b623769c2f19c95b19ce9320db89bbf91c0a
Edward Thai Phu Truong <edd.truong3@gmail.com>
2025-03-11 14:35:18 +1100

    Create app.py
    Constructed a python template for the weather app

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import *
+import requests 
+
+app = Flask(__name__)
+
+app.key = "77c62ab46f506f753e48c6fd6cf5f083"
+
+@app.route('/', methods = ["GET", "POST"]) 
+def index(): 
+  weather_data = None
+  if request.method == "POST":
+    city = request.form.get("city")
+    if city:
+      url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={app.key}&units=metric"
+
+      response = request.get(url)
+      # 200 OK status successful 
+      if response.status_code == 200:
+        weather_data = response.json
+      else:
+        weather_data = None
+  
+  return render_template("index.html", weather_data = weather_data) 
+
+if __name__ = "__main__":
+  app.run(debug = True)
