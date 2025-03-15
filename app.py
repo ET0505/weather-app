cabdf9990ea0e84e2ce8b6b7e33576f6be77969e
Edward Thai Phu Truong <edd.truong3@gmail.com>
2025-03-15 15:50:48 +1100

    Update app.py
    Fixed issues with code
    Verify that the connection works

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ import requests
 
 app = Flask(__name__)
 
+# API key
 app.key = "77c62ab46f506f753e48c6fd6cf5f083"
 
+# Decorator 
 @app.route('/', methods = ["GET", "POST"]) 
+
+
 def index(): 
   weather_data = None
   if request.method == "POST":
@@ -13,14 +17,18 @@ def index():
     if city:
       url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={app.key}&units=metric"
 
-      response = request.get(url)
+      response = requests.get(url)
+
       # 200 OK status successful 
       if response.status_code == 200:
-        weather_data = response.json
+        weather_data = response.json()
+    
       else:
         weather_data = None
   
+
   return render_template("index.html", weather_data = weather_data) 
 
-if __name__ = "__main__":
+
+if __name__ == "__main__":
   app.run(debug = True)
